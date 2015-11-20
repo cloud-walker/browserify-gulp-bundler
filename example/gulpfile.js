bcf00b2e5c0c6b781e0c940144cc59ce5aaf7b6e
Luca Barone <luca.b@docsity.com>
2015-11-20 17:53:55 +0100

    refactor with more testing

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -3,18 +3,30 @@ var sequence = require('gulp-sequence')
 var del      = require('del')
 var bundler  = require('../lib')
 
-gulp.task('default', sequence('clean', 'js:watch'))
-gulp.task('build', sequence('clean', 'js'))
+gulp.task('default', sequence('clean', 'multi:watch'))
+gulp.task('test', sequence('clean', 'nomatch', 'single', 'multi'))
 
 gulp.task('clean', function () {
   return del('dist')
 })
 
-const src = 'src/index.js'
-
-gulp.task('js', function () {
-  return bundler(src)
+gulp.task('nomatch', function () {
+  return bundler('something/index.js')
 })
-gulp.task('js:watch', function () {
-  return bundler(src, {watch: true})
+gulp.task('nomatch:watch', function (argument) {
+  return bundler('something/index.js', {watch: true})
+})
+
+gulp.task('single', function () {
+  return bundler('src/index.js')
+})
+gulp.task('single:watch', function () {
+  return bundler('src/index.js', {watch: true})
+})
+
+gulp.task('multi', function () {
+  return bundler('src/*.js')
+})
+gulp.task('multi:watch', function () {
+  return bundler('src/*.js', {watch: true})
 })
