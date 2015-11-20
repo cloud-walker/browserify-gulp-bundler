5dee16cbac95155b445545d753eda86dc675bbaa
Luca Barone <luca.b@docsity.com>
2015-11-20 15:40:25 +0100

    first js js:watch version

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,3 +1,19 @@
-var gulp = require('gulp');
+var gulp     = require('gulp');
+var sequence = require('gulp-sequence');
+var del      = require('del');
+var bundler  = require('../lib');
 
-gulp.task('default', []);
+gulp.task('default', sequence('clean', 'js:watch'));
+
+gulp.task('clean', function () {
+  return del('dist');
+});
+
+const src = 'index.js'
+
+gulp.task('js', function () {
+  return bundler(src);
+});
+gulp.task('js:watch', function () {
+  return bundler(src);
+});
