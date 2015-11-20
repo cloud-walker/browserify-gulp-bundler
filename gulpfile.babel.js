ed342d4992bf0f7381f9e99f65b42eb81fa6ef4a
Luca Barone <luca.b@docsity.com>
2015-11-20 17:34:12 +0100

    Add error reporting to gulp-babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import gulp     from 'gulp'
 import babel    from 'gulp-babel'
 import sequence from 'gulp-sequence'
+import plumber  from 'gulp-plumber'
+import notify   from 'gulp-notify'
 import watch    from 'gulp-watch'
 import del      from 'del'
 
@@ -16,6 +18,12 @@ gulp.task('clean', function () {
 
 gulp.task('babel', function () {
   return gulp.src(src)
+    .pipe(plumber({
+      errorHandler: notify.onError({
+        title  : 'Babel Error',
+        message: '<%= error.message %>'
+      })
+    }))
     .pipe(babel())
     .pipe(gulp.dest(dest))
 })
