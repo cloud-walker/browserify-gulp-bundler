d8aa6de39dd4170756d5b7466ef7e5011fbce7cc
Luca Barone <luca.b@docsity.com>
2015-11-20 17:35:18 +0100

    first working snapshot

---
+++ src/generateStream.js
@@ -0,0 +1,78 @@
+import browserify from 'browserify'
+import watchify   from 'watchify'
+import log        from './log'
+import source     from 'vinyl-source-stream'
+import through    from 'through2'
+import path       from 'path'
+import {dest}     from 'gulp'
+
+/**
+ * Generate the Browserify stream
+ *
+ * @param  {String} entry
+ * @param  {Object} opts  [Configuration options]
+ * @return {Object}       [Browserify stream]
+ */
+export default function (entry, opts = {}) {
+  const b = browserify({
+    cache       : {},
+    packageCache: {},
+    fullPaths   : true
+  })
+
+  b.add(entry)
+
+  if (opts.watch) {
+    b.plugin(watchify)
+    b.on('update', function () {
+      return generateBundle(b, entry)
+    })
+    b.on('time', function (time) {
+      return log(`${entry} ${parseTime(time)}`)
+    })
+  }
+
+  return generateBundle(b, entry)
+}
+
+/**
+ * Parse time
+ * @param  {Number} time [milliseconds]
+ * @return {String}
+ */
+function parseTime(time) {
+  if (time < 1000) {
+    time = `${time}ms`.green
+  } else if (time < 5000) {
+    time = `${time}ms`.yellow
+  } else {
+    time = `${time}ms`.red
+  }
+
+  return time
+}
+
+/**
+ * Finalize the bundle generation, used for both
+ * browserify and watchify.
+ * For now the basePath is the first folder of
+ * the entry.
+ * For now the dest folder is the dist folder.
+ *
+ * @param  {Object} b     [Browserify instance]
+ * @param  {String} entry
+ * @return {Object}       [Browserify stream]
+ */
+function generateBundle(b, entry) {
+  return b.bundle()
+    .on('error', function (err) {
+      console.log(`${err}`.red)
+    })
+    .pipe(source(entry))
+    .pipe(through.obj(function (file, enc, done) {
+      file.base = path.resolve(entry.split(path.sep).shift())
+      this.push(file)
+      done()
+    }))
+    .pipe(dest('dist'))
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,26 @@
-import glob   from 'glob'
-import colors from 'colors'
-import log    from './log.js'
+import glob           from 'glob'
+import colors         from 'colors'
+import log            from './log'
+import generateStream from './generateStream'
 
+/**
+ * @param  {String} pattern [Glob pattern]
+ * @param  {Object} opts    [Configuration options]
+ * @return {Mixed}         [Browserify stream or false]
+ */
 export default function (pattern, opts = {}) {
   const entries = glob.sync(pattern)
 
-  if (entries.length < 1) {
-  }
+  // console.log(entries)
+
   switch (entries.length) {
     case 0:
       log(`${'No bundle found'.yellow}...`)
-      break
+      return false
     case 1:
-
+      return generateStream(entries[0], opts)
     default:
-
+      log('Multiple bundles not supported yet..'.red)
+      return false
   }
-
-  return entries
-}
-
-function generateBundle(entry) {
-  const b = browserify({
-    cache       : {},
-    packageCache: {},
-    fullPaths   : true
-  })
-
-  b.add(entry)
-
-  return bundle(b, entry)
-}
-
-function bundle(b, entry) {
-  return b.bundle()
 }
